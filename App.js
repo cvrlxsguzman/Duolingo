9e3ab14eee3332cdc53e6efb33089fac65593156
cvrlxsguzman <carlosguzman31496@gmail.com>
2022-12-15 18:10:21 -0600

    Added AsyncStorage

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { View, Alert } from "react-native";
+import { View, Alert, ActivityIndicator } from "react-native";
+import AsyncStore from "@react-native-async-storage/async-storage/src";
 // Components
 import ImageMultipleChoiceQuestion from "./src/components/ImageMultipleChoiceQuestion";
 import OpenEndedQuestion from "./src/components/OpenEndedQuestion";
@@ -10,12 +11,15 @@ import questions from "./assets/data/allQuestions";
 import styles from "./App.styles";
 
 const App = () => {
+  // Set state variables
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [currentQuestion, setCurrentQuestion] = useState(
     questions[currentQuestionIndex]
   );
   const [lives, setLives] = useState(5);
+  const [hasLoaded, setHasLoaded] = useState(false);
 
+  // Update question index and Alert win
   useEffect(() => {
     if (currentQuestionIndex >= questions.length) {
       Alert.alert("You won");
@@ -25,15 +29,30 @@ const App = () => {
     }
   }, [currentQuestionIndex]);
 
-  const onCorrect = () => {
-    setCurrentQuestionIndex(currentQuestionIndex + 1);
-  };
+  // Load data
+  useEffect(() => {
+    getData().then((r) => console.log("Game Data loaded"));
+  }, []);
 
+  // Save data
+  useEffect(() => {
+    if (hasLoaded) {
+      storeData().then((r) => console.log("Game Saved!"));
+    }
+  }, [lives, currentQuestionIndex, hasLoaded]);
+
+  // Restart game
   const restart = () => {
     setLives(5);
     setCurrentQuestionIndex(0);
   };
 
+  // Update current question index on correct
+  const onCorrect = () => {
+    setCurrentQuestionIndex(currentQuestionIndex + 1);
+  };
+
+  // Alert wrong and start over when player loses
   const onWrong = () => {
     if (lives <= 1) {
       Alert.alert("Game Over", "Try again", [
@@ -47,6 +66,33 @@ const App = () => {
       setLives(lives - 1);
     }
   };
+
+  // Store data function
+  const storeData = async () => {
+    await AsyncStore.setItem("lives", lives.toString());
+    await AsyncStore.setItem(
+      "currentQuestionIndex",
+      currentQuestionIndex.toString()
+    );
+  };
+
+  // Load data function
+  const getData = async () => {
+    const loadedLives = await AsyncStore.getItem("lives");
+    const loadedIndex = await AsyncStore.getItem("currentQuestionIndex");
+    if (loadedLives) {
+      setLives(parseInt(loadedLives));
+    }
+    if (loadedIndex) {
+      setCurrentQuestionIndex(parseInt(loadedIndex));
+    }
+
+    setHasLoaded(true);
+  };
+
+  if (!hasLoaded) {
+    return <ActivityIndicator />;
+  }
 
   return (
     <View style={styles.root}>
